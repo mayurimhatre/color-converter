c5d073121450ebb940d6da7148711f3aabbef5a2
Rob Layzell <robert.layzell@ca.com>
2019-02-08 12:49:14 -0800

    More debugging!

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # These should be fine set at their default values.
 # If we want a user to be able to set them, we could update the Jenkins Plugin so 
 # they could be configured as necessary.
-# export IASTAGENT_LOGGING_STDERR_LEVEL=info
+export IASTAGENT_LOGGING_STDERR_LEVEL=info
 # export IASTAGENT_LOGGING_FILE_ENABLED=true
 # export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
 # export IASTAGENT_LOGGING_FILE_LEVEL=info
@@ -13,7 +13,5 @@
 # export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
 # export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=$1
 # export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=$2
-
-echo IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED:${IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED}
 
 node -r ./agent_nodejs_linux64 app/server.js
